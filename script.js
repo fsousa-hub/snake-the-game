da945aeb8017618503cdf8f29f565c685159094e
fsousa <onics.aulas@gmail.com>
2023-02-04 14:15:59 -0300

    Definido fim de jogo

--- script.js
+++ script.js
@@ -85,6 +85,17 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if(snake[0].y > 0 && direction == "up") snake[0].y = 16 * box;
+
+    /**
+     *  Criar um loop tipo for, detecar colisão entre cabeça cobrinha e parte do corpo da cobrinha
+     *  Acionar uma alerta "alert" e informar fim de jogo "Game Over"
+    */
+   for (i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+   }
     
     criarBG(); // cria backgroud
     criarCobrinha(); // cria a cobrinha
